e9e2395c09fe9ae2f0793c62362fd7dd17e16be5
TMMXTF\jlara <jlara@thefives.com.mx>
2024-02-26 19:25:21 -0500

    se agrego  la info del menu ya es lo mas parecido pero hace falta ajustra detalles  el boton de ayuda aun no tiene funcionalidad

--- the-fives-hotels/src/components/Menu/Menu.js
+++ the-fives-hotels/src/components/Menu/Menu.js
@@ -1,205 +1,202 @@
-"use client"
-import Droplist from '../droplist/Droplist'
-import DoubleDropList from '../DoubleDropList/DoubleDropList'
-import Link from 'next/link'
-import Image from 'next/image'
-import './Menu.css'
+"use client";
+import Droplist from "../droplist/Droplist";
+import DoubleDropList from "../DoubleDropList/DoubleDropList";
+import Link from "next/link";
+import Image from "next/image";
+import "./Menu.css";
 
+export default function Menu({ setIsOpen, isOpen }) {
+  const menu = {
+    title: "DESTINATIONS AND RESORTS",
+    items: [
+      {
+        name: "THE FIVES BEACH HOTEL & RESIDENCES",
+        url: "#",
+      },
+      {
+        name: "THE FIVES DOWNTOWN HOTEL & RESIDENCES",
+        url: "#",
+      },
+      {
+        name: "THE FIVES OCEANFRONT PUERTO MORELOS",
+        url: "#",
+      },
+      {
+        name: "THE BEACHFRONT BY THE FIVES HOTELS",
+        url: "#",
+      },
+      {
+        name: "THE BEACHFRONT BY THE FIVES HOTE",
+        url: "#",
+      },
+      {
+        name: "DESTINATIONS",
+        url: "#",
+      },
+    ],
+  };
 
+  const restaurants = {
+    title: "RESTAURANTS",
+    items: [
+      {
+        name: "THE FIVES BEACH HOTEL & RESIDENCES",
 
-export default function Menu({ setIsOpen, isOpen}) {
-    const menu =
-        {   
-            title: "DESTINATIONS AND RESORTS",
-            items: [
-                {
-                    name: "THE FIVES BEACH HOTEL & RESIDENCES",
-                    url: "#"
-                },
-                {
-                    name: "THE FIVES DOWNTOWN HOTEL & RESIDENCES",
-                    url: "#"
-                },
-                {
-                    name: "THE FIVES OCEANFRONT PUERTO MORELOS",
-                    url: "#"
-                },
-                {
-                    name: "THE BEACHFRONT BY THE FIVES HOTELS",
-                    url: "#"
-                },
-                {
-                    name: "THE BEACHFRONT BY THE FIVES HOTE",
-                    url: "#"
-                },
-                {
-                    name: "DESTINATIONS",
-                    url: "#"
-                },
-            ]
-        }
+        restaurants: [
+          {
+            name: "AREZZO",
+            url: "#",
+            description: "Italian Cuisine",
+          },
+          {
+            name: "BAR PALMAS",
+            url: "#",
+            description: "Bar",
+          },
+          {
+            name: "BREEZE BURGUER",
+            url: "#",
+            description: "Burger Restaurant",
+          },
+          {
+            name: "CANTINA 1910",
+            url: "#",
+            description: "Mexican Cuisine",
+          },
+          {
+            name: "MESTIZO",
+            url: "#",
+            description: "Mexican Cuisine",
+          },
+          {
+            name: "THE GIN BAR",
+            url: "#",
+            description: "Bar",
+          },
+          {
+            name: "ZKY BAR",
+            url: "#",
+            description: "Bar",
+          },
+          {
+            name: "FLAVOURS",
+            url: "#",
+            description: "International Cuisine",
+          },
+          {
+            name: "OKA SUSHI",
+            url: "#",
+            description: "Japanese Cuisine",
+          },
+          {
+            name: "KOH THAI - WOK CUISINE",
+            url: "#",
+            description: "Thai Cuisine",
+          },
+          {
+            name: "SEA OLIVE - CUISINE DU SOLEIL",
+            url: "#",
+            description: "Mediterranean Cuisine",
+          },
+          {
+            name: "AREZZO - ITALIAN RESTAURANT",
+            url: "#",
+            description: "Italian Cuisine",
+          },
+          {
+            name: "NATURA - DAY CLUB AND GRILL",
+            url: "#",
+            description: "Grill",
+          },
+          {
+            name: "LA BRASSERIE - BISTRO CUISINE",
+            url: "#",
+            description: "Bistro Cuisine",
+          },
+          {
+            name: "LIZZARDS - HEALTHIER & BETTER",
+            url: "#",
+            description: "Healthy Food",
+          },
+          {
+            name: "MARLEY COFFEE",
+            url: "#",
+            description: "Coffee Shop",
+          },
+        ],
+      },
+      {
+        name: "THE FIVES DOWNTOWN",
 
-        const restaurants = {
-          title: "RESTAURANTS",
-          items: [
-            {
-              name: "THE FIVES BEACH HOTEL & RESIDENCES",
+        restaurants: [
+          {
+            name: "EL TIGRE - CANTINA MEXICANA",
+            url: "#",
+            description: "Mexican Cantina",
+          },
+          {
+            name: "MERCATTO",
+            url: "#",
+            description: "Italian Cuisine",
+          },
+          {
+            name: "FIVES ROOFTOP",
+            url: "#",
+            description: "Rooftop Restaurant",
+          },
+          {
+            name: "LOLA - ROOFTOP RESTAURANT",
+            url: "#",
+            description: "Rooftop Restaurant",
+          },
+        ],
+      },
+      {
+        name: "THE FIVES OCEANFRONT",
+        restaurants: [
+          {
+            name: "MANGLAR LOBBY BAR",
+            url: "#",
+            description: "Bar",
+          },
+          {
+            name: "ROMARLEY BEACH HOUSE",
+            url: "#",
+            description: "Beach House",
+          },
+          {
+            name: "K'IIN ROOFTOP",
+            url: "#",
+            description: "Rooftop",
+          },
+          {
+            name: "ROMARLEY POOL BAR",
+            url: "#",
+            description: "Pool Bar",
+          },
+          {
+            name: "AL MARE - COCINA ITALIANA",
+            url: "#",
+            description: "Italian Cuisine",
+          },
+        ],
+      },
+    ],
+  };
 
-              restaurants: [
-                {
-                  name: "AREZZO",
-                  url: "#",
-                  description: "Italian Cuisine",
-                },
-                {
-                  name: "BAR PALMAS",
-                  url: "#",
-                  description: "Bar",
-                },
-                {
-                  name: "BREEZE BURGUER",
-                  url: "#",
-                  description: "Burger Restaurant",
-                },
-                {
-                  name: "CANTINA 1910",
-                  url: "#",
-                  description: "Mexican Cuisine",
-                },
-                {
-                  name: "MESTIZO",
-                  url: "#",
-                  description: "Mexican Cuisine",
-                },
-                {
-                  name: "THE GIN BAR",
-                  url: "#",
-                  description: "Bar",
-                },
-                {
-                  name: "ZKY BAR",
-                  url: "#",
-                  description: "Bar",
-                },
-                {
-                  name: "FLAVOURS",
-                  url: "#",
-                  description: "International Cuisine",
-                },
-                {
-                  name: "OKA SUSHI",
-                  url: "#",
-                  description: "Japanese Cuisine",
-                },
-                {
-                  name: "KOH THAI - WOK CUISINE",
-                  url: "#",
-                  description: "Thai Cuisine",
-                },
-                {
-                  name: "SEA OLIVE - CUISINE DU SOLEIL",
-                  url: "#",
-                  description: "Mediterranean Cuisine",
-                },
-                {
-                  name: "AREZZO - ITALIAN RESTAURANT",
-                  url: "#",
-                  description: "Italian Cuisine",
-                },
-                {
-                  name: "NATURA - DAY CLUB AND GRILL",
-                  url: "#",
-                  description: "Grill",
-                },
-                {
-                  name: "LA BRASSERIE - BISTRO CUISINE",
-                  url: "#",
-                  description: "Bistro Cuisine",
-                },
-                {
-                  name: "LIZZARDS - HEALTHIER & BETTER",
-                  url: "#",
-                  description: "Healthy Food",
-                },
-                {
-                  name: "MARLEY COFFEE",
-                  url: "#",
-                  description: "Coffee Shop",
-                },
-              ],
-            },
-            {
-              name: "THE FIVES DOWNTOWN",
-
-              restaurants: [
-                {
-                  name: "EL TIGRE - CANTINA MEXICANA",
-                  url: "#",
-                  description: "Mexican Cantina",
-                },
-                {
-                  name: "MERCATTO",
-                  url: "#",
-                  description: "Italian Cuisine",
-                },
-                {
-                  name: "FIVES ROOFTOP",
-                  url: "#",
-                  description: "Rooftop Restaurant",
-                },
-                {
-                  name: "LOLA - ROOFTOP RESTAURANT",
-                  url: "#",
-                  description: "Rooftop Restaurant",
-                },
-              ],
-            },
-            {
-              name: "THE FIVES OCEANFRONT",
-              restaurants: [
-                {
-                  name: "MANGLAR LOBBY BAR",
-                  url: "#",
-                  description: "Bar",
-                },
-                {
-                  name: "ROMARLEY BEACH HOUSE",
-                  url: "#",
-                  description: "Beach House",
-                },
-                {
-                  name: "K'IIN ROOFTOP",
-                  url: "#",
-                  description: "Rooftop",
-                },
-                {
-                  name: "ROMARLEY POOL BAR",
-                  url: "#",
-                  description: "Pool Bar",
-                },
-                {
-                  name: "AL MARE - COCINA ITALIANA",
-                  url: "#",
-                  description: "Italian Cuisine",
-                },
-              ],
-            },
-          ],
-        };
-
-    const PreCheckin= {
-        title: "PRE-CHECK-IN",
-        items: [
-            {
-                name: "THE FIVES BEACH",
-                url: "#"
-            },
-            {
-                name: "THE FIVES OCEANFRONT",
-                url: "#"
-            },
-        ]
-    }
+  const PreCheckin = {
+    title: "PRE-CHECK-IN",
+    items: [
+      {
+        name: "THE FIVES BEACH",
+        url: "#",
+      },
+      {
+        name: "THE FIVES OCEANFRONT",
+        url: "#",
+      },
+    ],
+  };
 
   return (
     <section className="menu">
@@ -254,7 +251,7 @@ export default function Menu({ setIsOpen, isOpen}) {
         </article>
       </div>
       <div className="menu__rigth-side">
-        <article className='menu__header'>
+        <article className="menu__header">
           <Image
             src="/assets/logo.svg"
             alt="The Fives Hotels logo"
@@ -272,31 +269,79 @@ export default function Menu({ setIsOpen, isOpen}) {
             </a>
           </section>
           <section className="header__language ">
-        <a href="#" className="border__white">
-          <b className="color__white">
-            EN
-          </b>
-        </a>
-        <a href="#">
-          <b className="color__white">
-          
-            ES
-          </b>
-        </a>
-      </section>
-      <section className="header__help">
-        <button className={"border__white"}>
-          <Image
-        
-            src="/assets/question-icon.svg"
-            alt="Help icon"
-            width={20}
-            height={20}
-          />
-        </button>
-      </section> 
-    
+            <a href="#" className="border__white">
+              <b className="color__white">EN</b>
+            </a>
+            <a href="#">
+              <b className="color__white">ES</b>
+            </a>
+          </section>
+          <section className="header__help">
+            <button className={"border__white"}>
+              <Image
+                src="/assets/question-icon.svg"
+                alt="Help icon"
+                width={20}
+                height={20}
+              />
+            </button>
+          </section>
         </article>
+        <section className="menu__body">
+          <ul className="menu__sections">
+            <li className="menu__section">
+              <Link className="menu__section-link" href="#">
+                BLOG
+              </Link>
+            </li>
+            <li className="menu__section">
+              <Link className="menu__section-link" href="#">
+                GALLERY
+              </Link>
+            </li>
+            <li className="menu__section">
+              <Link className="menu__section-link" href="#">
+                SENSE OF JOY
+              </Link>
+            </li>
+          </ul>
+          <article className="menu__contact">
+            <h3 className="menu__contact-title">
+              CONTACT US
+            </h3>
+            
+            <a className=" menu__contact-phone" href="tel:+11234567890">
+              <p className="color__white">USA & CAN:</p>
+              <b className="color__white">+1 123-456-7890</b>
+            </a>
+            <a className="menu__contact-phone " href="tel:+521234567890">
+              <p className="color__white">MEX:</p>
+              <b className="color__white"> +52 123-456-7890</b>
+            </a>
+            <a className="menu__contact-phone " href="tel:+521234567890">
+              <p className="color__white">SPAIN TOOL FREE:</p>
+              <b className="color__white"> +52 123-456-7890</b>
+            </a>
+            <a className="menu__contact-phone " href="tel:+521234567890">
+              <p className="color__white">BRAZIL TOOL FREE</p>
+              <b className="color__white"> +52 123-456-7890</b>
+            </a>
+            <a className="menu__contact-phone " href="tel:+521234567890">
+              <p className="color__white">REST OF THE WORLD</p>
+              <b className="color__white"> +52 123-456-7890</b>
+            </a>
+            <address className="menu__contact-phone">
+            ACCESO XCALACOCO S/N. FRACC. EL LIMONAR 1, PLAYA DEL CARMEN, QUINTANA ROO, C.P. 77710
+
+            </address>
+
+            <a className="menu__contact-phone " href="tel:+521234567890">
+             
+              <b className="color__white"> reservationsinfo@thefiveshotels.com</b>
+            </a>
+            
+          </article>
+        </section>
       </div>
     </section>
   );
